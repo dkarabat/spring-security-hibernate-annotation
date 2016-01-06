be830045b16102989f06e228b9dc0bb2c39dfa59
dkarabat <dkarabat13@gmail.com>
2016-01-06 13:58:50 +0200

    Initial commit

---
+++ src/main/java/com/mkyong/config/AppConfig.java
@@ -0,0 +1,69 @@
+package com.mkyong.config;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.util.Properties;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.mkyong.*" })
+@EnableTransactionManagement
+@Import({ SecurityConfig.class })
+public class AppConfig {
+
+	@Bean
+    public SessionFactory sessionFactory() {
+        LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource());
+        builder
+        	.scanPackages("com.mkyong.users.model")
+            .addProperties(getHibernateProperties());
+
+        return builder.buildSessionFactory();
+    }
+
+	private Properties getHibernateProperties() {
+        Properties prop = new Properties();
+        prop.put("hibernate.format_sql", "true");
+        prop.put("hibernate.show_sql", "true");
+        prop.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		prop.put("hibernate.hbm2ddl.auto", "create");
+        return prop;
+    }
+	
+	@Bean(name = "dataSource")
+	public BasicDataSource dataSource() {
+		
+		BasicDataSource ds = new BasicDataSource();
+	    ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_0f9111f34b140a1?reconnect=true");
+		ds.setUsername("bd81c53ff452ee");
+		ds.setPassword("642c4c27");
+		return ds;
+	}
+	
+	@Bean
+    public HibernateTransactionManager txManager() {
+        return new HibernateTransactionManager(sessionFactory());
+    }
+		
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/pages/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+}
